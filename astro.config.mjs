77ab420d14483f55cb9b16b9999f7abc3d584e77
Diego Ochoa <daochoa@pucp.edu.pe>
2025-11-16 12:15:44 -0500

    Fix astro.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/',
   integrations: [react()],
 
   vite: {
